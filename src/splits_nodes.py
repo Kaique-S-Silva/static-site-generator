e8ea7328b671587ac146b58b1b8e6e46d71dfbad
Merthiz <k16ksds@gmail.com>
2026-06-12 08:46:16 -0300

    refactor: split_nodes_image and split_nodes_link to share a single helper function

--- src/splits_nodes.py
+++ src/splits_nodes.py
@@ -20,29 +20,30 @@ def split_nodes_delimeter(old_nodes: list[TextNode], delimeter: str, text_type: 
                     return_list.append(TextNode(i_text[j], text_type))
     return return_list
 
-def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+
+def _split_nodes_markup(old_nodes, extract_func, text_type):
     return_list = []
 
     for i in old_nodes:
         if i.text_type != TextType.TEXT:
             return_list.append(i)
         else:
-            images = extract_markdown_images(i.text)
-            if len(images) == 0:
+            nodes = extract_func(i.text)
+            if len(nodes) == 0:
                 return_list.append(i)
             else:
                 original_text = i.text
 
-                for image in images:
-                    alt = image[0]
-                    url = image[1]
-                    target_markdown = f"![{alt}]({url})"
+                for node in nodes:
+                    alt = node[0]
+                    url = node[1]
+                    target_markdown = f"![{alt}]({url})" if text_type == TextType.IMAGE else f"[{alt}]({url})"
 
                     sections = original_text.split(target_markdown, 1)
 
                     if sections[0] != "":
                         return_list.append(TextNode(sections[0], TextType.TEXT))
-                    return_list.append(TextNode(alt, TextType.IMAGE, url))
+                    return_list.append(TextNode(alt, text_type, url))
 
                     original_text = sections[1]
 
@@ -50,34 +51,8 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
                     return_list.append(TextNode(original_text, TextType.TEXT))
 
     return return_list
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    return _split_nodes_markup(old_nodes, extract_markdown_images, TextType.IMAGE)
 
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
-    return_list = []
-
-    for i in old_nodes:
-        if i.text_type != TextType.TEXT:
-            return_list.append(i)
-        else:
-            links = extract_markdown_links(i.text)
-            if len(links) == 0:
-                return_list.append(i)
-            else:
-                original_text = i.text
-
-                for link in links:
-                    alt = link[0]
-                    url = link[1]
-                    target_markdown = f"[{alt}]({url})"
-
-                    sections = original_text.split(target_markdown, 1)
-
-                    if sections[0] != "":
-                        return_list.append(TextNode(sections[0], TextType.TEXT))
-                    return_list.append(TextNode(alt, TextType.LINK, url))
-
-                    original_text = sections[1]
-
-                if original_text != "":
-                    return_list.append(TextNode(original_text, TextType.TEXT))
-
-    return return_list
+    return _split_nodes_markup(old_nodes, extract_markdown_links, TextType.LINK)
